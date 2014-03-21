969a841a002a6c00a7bf76c1966617b392728d92
James Stubblefield <jameswilliamiii@gmail.com>
2014-03-21 12:12:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThriveMarket::Application.config.secret_key_base = 'e66e48d2d187eb5f155a7c85dd7f842bcd8fc137d78772e97841c4878dbba3fc440694a6aa7d6c5f29a9fc83e9a9522c785f408d60f88a490aee87cb88d3b22e'
