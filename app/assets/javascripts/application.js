10a6f1e0c9b7f539de570ebcfbb6da03725bff67
James Stubblefield <jameswilliamiii@gmail.com>
2014-03-24 11:05:47 -0500

    move nav to bottom on medium and large screens

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,12 @@
 //= require foundation
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function(){ $(document).foundation();
+  // Smooth scroll
+  $('a.smooth').click(function(){
+      $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top - 65
+      }, 500);
+      return false;
+  });
+});
