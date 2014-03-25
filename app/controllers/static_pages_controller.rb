68a2318feec5c4d73e0e1a7b55eeadea06678888
James Stubblefield <jameswilliamiii@gmail.com>
2014-03-25 12:11:45 -0500

    added flash messages and hooked up form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def thanks
+    redirect_to root_url, notice: "Thank you, we will get back to you as soon as possible."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ThriveMarket::Application.routes.draw do
 
   root 'static_pages#home'
 
-  get "static_pages/home"
+  get "/home" => "static_pages#home", as: :home
+  get "/thanks" => "static_pages#thanks", as: :thanks
 
 end
