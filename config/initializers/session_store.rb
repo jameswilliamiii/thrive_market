a25da8519a00dfb66c9ff21de5cf5f362455ad8c
James Stubblefield <jameswilliamiii@gmail.com>
2014-03-27 00:26:40 -0500

    remove session expiration from rails sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ThriveMarket::Application.config.session_store :cookie_store, key: '_thrive_market_session', :expire_after => 5.minutes
+ThriveMarket::Application.config.session_store :cookie_store, key: '_thrive_market_session'
