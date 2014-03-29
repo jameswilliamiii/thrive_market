92f46171eef7b892e41f8023aec36a15661c7dab
James Stubblefield <jameswilliamiii@gmail.com>
2014-03-28 23:14:19 -0500

    add sitemap

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'quiet_assets'
+  gem 'sitemap_generator'
 end
 
 group :production do
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ ThriveMarket::Application.routes.draw do
   root 'static_pages#home'
 
   get  "/home"               => "static_pages#home",     as: :home
-  get  "/thanks"             => "static_pages#thanks",   as: :thanks
   post "/phone_numbers"      => "phone_numbers#create"
   post  "webhooks/sms"       => 'webhooks#incoming_text'
   post "survey_items/create" => 'survey_items#create',   as: :survey_items
@@ -14,7 +13,7 @@ ThriveMarket::Application.routes.draw do
   resources :sessions, only: [ :create ]
   resources :updates,  only: [ :create, :destroy ]
 
-  get "admin"         => "admin#index",         as: :admin
+  get "admin"               => "admin#index",         as: :admin
   get "admin/survey_items"  => "admin#survey_items",  as: :admin_survey_items
   get "admin/phone_numbers" => "admin#phone_numbers", as: :admin_phone_numbers
   get "admin/updates"       => "admin#updates",       as: :admin_updates
