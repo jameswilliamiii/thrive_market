58d3de2173dd950e9fe900f3860e7986a412fa65
James Stubblefield <jameswilliamiii@gmail.com>
2014-06-25 14:12:21 -0500

    remove add on text to sms content

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -8,7 +8,7 @@ class PhoneNumber < ActiveRecord::Base
     client.account.sms.messages.create(
       from: ENV['TWILO_FROM'],
       to: self.number,
-      body: content + " You can reply STOP to be removed from these texts."
+      body: content
       )
     rescue Twilio::REST::RequestError => e
       puts "ERROR: #{e.message}, #{self.number}"
